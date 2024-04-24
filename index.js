5a2e10de5375343b9b3964fd032e919a8a211fe4
TshireletsoRakoma <156686384+TshireletsoRakoma@users.noreply.github.com>
2024-04-25 00:21:06 +0200

    adding functions

--- index.js
+++ index.js
@@ -2,3 +2,16 @@
 import {getTasks, createNewTask, patchTask, putTask, deleteTask} from './utils/taskFunctions.js';
 // TASK: import initialData
 import {initialData} from './initialData.js';
+/*************************************************************************************************************************************************
+ * 
+ * **********************************************************************************************************************************************/
+
+// Function checks if local storage already has data, if not it loads initialData to localStorage
+function initializeData() {
+  if (!localStorage.getItem('tasks')) {
+    localStorage.setItem('tasks', JSON.stringify(initialData)); 
+    localStorage.setItem('showSideBar', 'true')
+  } else {
+    console.log('Data already exists in localStorage');
+  }
+}
